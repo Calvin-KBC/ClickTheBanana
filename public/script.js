5bd9731b1143ae25af65b546efec2b64d8e4a108
Calvin-KBC <calvin77@hotmail.de>
2026-07-14 23:55:02 +0200

    add SQL and global counter, remove local counter

--- public/script.js
+++ public/script.js
@@ -1,12 +1,24 @@
 const button = document.getElementById("bananaButton");
 var bananaAudio = document.getElementById("bananaAudio");
+const counter = document.getElementById("counter");
 
-let bananas = 0;
+fetch("/counter")
+    .then(response => response.json())
+    .then(data => {
+        counter.textContent = data.counter;
+    });
+
 function addBanana(){
-    bananas++;
+    fetch("/click", {
+        method:"POST"
+    })
+        .then(response => response.json())
+        .then(data => {
+            counter.textContent = data.counter;
+        })
+
     bananaAudio.currentTime = 0;
     bananaAudio.play();
-    counter.textContent = bananas;
 }
 
 button.addEventListener("click", addBanana);
--- server.js
+++ server.js
@@ -1,7 +1,54 @@
 const express = require("express");
+const Database = require("better-sqlite3");
+const db = new Database("banana.db");
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS bananas (
+        id INTEGER PRIMARY KEY,
+        counter INTEGER
+    );
+`);
+
+db.exec(`
+    INSERT OR IGNORE INTO bananas (id, counter)
+    VALUES (1, 0);
+`);
+
 const app = express();
 
 app.use(express.static("public"));
+
+app.get("/counter", (req, res) => {
+    const row = db.prepare(`
+    SELECT counter
+    FROM bananas
+    WHERE id = 1
+`).get();
+
+    res.json({
+        counter: row.counter
+    });
+});
+
+app.post("/click", (req, res) => {
+    const row = db.prepare(`
+        SELECT counter
+        FROM bananas
+        WHERE id = 1
+    `).get();
+
+    const newCounter = row.counter + 1;
+
+    db.prepare(`
+        UPDATE bananas
+        SET counter = ?
+        WHERE id = 1
+    `).run(newCounter);
+
+    res.json({
+        counter: newCounter
+    });
+});
 
 app.listen(3000, () => {
     console.log("Server läuft auf http://localhost:3000");
